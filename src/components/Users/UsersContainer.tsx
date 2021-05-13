7e8b8ba7611c65654b53c23e18a4314d0d389453
login <kakvospitat@gmail.com>
2021-05-13 18:42:36 +0300

    13.05.2021-83 - React JS - подключаем reselect (reselect часть 3)

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -16,8 +16,7 @@ import {
     getCurrentPage, getFollowingInProgress,
     getIsFetching,
     getPageSize,
-    getTotalUsersCount,
-    GetUsers
+    getTotalUsersCount, GetUsers,
 } from "../../redux/users-selectors";
 
 
@@ -31,17 +30,17 @@ export type initialStateType = {
     setCurrentPage: (currentPage: number) => void,
     isFetching: boolean
     followingInProgress: Array<any>
-    getUsers: (currentPage: number, pageSize: number) => void
-
+    GetUsers: (currentPage: number, pageSize: number) => void
+    // GetUsersSuperSelector: (currentPage: number, pageSize: number) => void
 }
 
 class UsersContainer extends React.Component<initialStateType> {
     componentDidMount() {
-        this.props.getUsers(this.props.currentPage, this.props.pageSize);
+        this.props.GetUsers(this.props.currentPage, this.props.pageSize);
     }
 
     onPageChange = (pageNumber: number) => {
-        this.props.getUsers(pageNumber, this.props.pageSize);
+        this.props.GetUsers(pageNumber, this.props.pageSize);
     }
 
     render() {
@@ -63,34 +62,25 @@ class UsersContainer extends React.Component<initialStateType> {
     }
 }
 
-// function mapStateToProps(state: AppStateType) {
-//     return {
-//         users: state.usersPage.users,
-//         pageSize: state.usersPage.pageSize,
-//         totalUsersCount: state.usersPage.totalUsersCount,
-//         currentPage: state.usersPage.currentPage,
-//         isFetching: state.usersPage.isFetching,
-//         followingInProgress: state.usersPage.followingInProgress
-//     }
-// }
-
-//делаем все тоже самое только-доступ к стейту через функцию
 function mapStateToProps(state: AppStateType) {
     return {
-        users: GetUsers(state),//назв.функции совпадает с экшеном!!!!
+        users: GetUsers(state),
+        // users: GetUsersSuperSelector(state),
         pageSize: getPageSize(state),
         totalUsersCount: getTotalUsersCount(state),
         currentPage: getCurrentPage(state),
         isFetching: getIsFetching(state),
-        followingInProgress:getFollowingInProgress(state)
+        followingInProgress: getFollowingInProgress(state)
     }
 }
 
-
 export default compose<React.ComponentType>(
-    connect(mapStateToProps, {follow, unfollow, setCurrentPage,
-        toggleFollowingProgress, getUsers: requestUsers}))
+    connect(mapStateToProps, {
+        follow, unfollow, setCurrentPage,
+        toggleFollowingProgress, GetUsers: requestUsers
+    }))
 (UsersContainer)
+
 
 
 
